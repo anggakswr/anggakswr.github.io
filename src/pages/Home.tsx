947d5efb00603802c5b55f242e4446f34d92e19b
angga <angga@primaku.com>
2023-12-22 14:45:13 +0700

    Sidebar menu design 2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,44 @@
+import { useState } from "react";
+
 const Home = () => {
   return (
     <div className="bg-black min-h-screen box-center flex-col gap-8">
-      <Menu />
-      <Menu bOdd />
-      <Menu />
-      <Menu bOdd />
+      {aMenus.map((sMenu, nIndex) => (
+        <Menu sText={sMenu} bOdd={!(nIndex % 2)} />
+      ))}
     </div>
   );
 };
 
-const Menu = ({ bOdd }: { bOdd?: boolean }) => {
+const aMenus = ["HOME", "PROJECTS", "CONTENT", "CONTACT"];
+
+const Menu = ({ sText, bOdd }: { sText: string; bOdd?: boolean }) => {
+  const [bHover, setBHover] = useState(false);
+
+  const sBoxCSS = "absolute top-0 left-0 w-full h-full";
+  const sBox1CSS = "z-10";
+  const sBox1Anim = bHover ? "rotate-0 bg-white" : "rotate-12 bg-red-400";
+  const sBox2CSS = "bg-red-700 skew-x-12 hover:";
+  const sBox2Anim = bHover ? "rotate-12" : "";
+
+  const sTextAnim = bHover ? "" : "text-white";
+
   return (
-    <div className="relative w-[100px] h-[50px] box-center">
-      <div className="text-white font-bold">Home</div>
+    <div
+      className="relative w-[100px] h-[50px] box-center"
+      onMouseOver={() => setBHover(true)}
+      onMouseOut={() => setBHover(false)}
+    >
+      <p className={`relative z-20 drop-shadow-md text-xl ${sTextAnim}`}>
+        {sText}
+      </p>
 
       <div className={`absolute inset-0 ${bOdd ? "rotate-180" : ""}`}>
-        <div className="absolute left-0 -top-4 w-1 h-[80px] bg-white" />
-        <div className="absolute right-0 -bottom-4 w-1 h-[60px] bg-white rotate-12" />
-        <div className="absolute top-0 -left-4 w-[120px] h-1 bg-white rotate-12" />
-        <div className="absolute bottom-0 -left-4 w-[120px] h-1 bg-white -rotate-3" />
+        {/* box */}
+        <div className={`${sBoxCSS} ${sBox1CSS} ${sBox1Anim}`} />
+
+        {/* parallelogram */}
+        <div className={`${sBoxCSS} ${sBox2CSS} ${sBox2Anim}`} />
       </div>
     </div>
   );
