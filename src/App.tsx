68db02430e664125708cedce0a8c52e612f2e4ef
angga <angga@primaku.com>
2023-12-22 13:54:21 +0700

    Design 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import sUpperChar1 from "./helpers/sUpperChar1";
-import Sidebar from "./components/Sidebar";
-import MobileHeader from "./components/MobileHeader";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
@@ -23,25 +21,9 @@ function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <main className="px-4 py-4 md:py-8 md:w-3/4 mx-auto">
-        <MobileHeader />
-
-        <div className="min-h-screen flex gap-x-8 mt-20 md:mt-0 mb-4">
-          {/* sidebar */}
-          <Sidebar />
-
-          {/* body */}
-          <section className="md:ml-64 flex-1">
-            <Outlet />
-          </section>
-        </div>
-
-        <hr />
-
-        <footer className="text-center text-gray-600 text-sm py-2">
-          Made with Vite by Kamiswara Angga W.
-        </footer>
-      </main>
+      <div>
+        <Outlet />
+      </div>
     </QueryClientProvider>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,26 @@
-import Bio from "../components/home/Bio";
-import Experiences from "../components/home/Experiences";
-import Skills from "../components/home/Skills";
-
 const Home = () => {
   return (
-    <>
-      <Bio />
+    <div className="bg-black min-h-screen box-center flex-col gap-8">
+      <Menu />
+      <Menu bOdd />
+      <Menu />
+      <Menu bOdd />
+    </div>
+  );
+};
 
-      <hr className="mb-4" />
+const Menu = ({ bOdd }: { bOdd?: boolean }) => {
+  return (
+    <div className="relative w-[100px] h-[50px] box-center">
+      <div className="text-white font-bold">Home</div>
 
-      <Experiences />
-
-      <hr className="mb-4" />
-
-      <Skills />
-    </>
+      <div className={`absolute inset-0 ${bOdd ? "rotate-180" : ""}`}>
+        <div className="absolute left-0 -top-4 w-1 h-[80px] bg-white" />
+        <div className="absolute right-0 -bottom-4 w-1 h-[60px] bg-white rotate-12" />
+        <div className="absolute top-0 -left-4 w-[120px] h-1 bg-white rotate-12" />
+        <div className="absolute bottom-0 -left-4 w-[120px] h-1 bg-white -rotate-3" />
+      </div>
+    </div>
   );
 };
 
